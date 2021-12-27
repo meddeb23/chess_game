b2464349ec9151886ab768a05046453e1bfcc6e8
meddeb23 <dalimeddeb132@gmial.com>
2021-12-27 18:34:53 +0100

    adding pawn and rock moves

--- gameState.py
+++ gameState.py
@@ -6,10 +6,11 @@ class GameState():
             ["--", "--", "--", "--", "--", "--", "--", "--"],
             ["--", "--", "--", "--", "--", "--", "--", "--"],
             ["--", "--", "--", "--", "--", "--", "--", "--"],
-            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["wR", "bp", "--", "--", "--", "--", "--", "--"],
             ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],
             ["wR", "wN", "wB", "wQ", "wk", "wB", "wN", "wR"],
         ]
+        self.dimension = 8
         self.selectedSq = None
         self.playerSelections = []  # track the user's squar selection history
         self.isWhiteTurn = True
@@ -18,33 +19,36 @@ class GameState():
     def selectPiece(self, coord):
         player = ["b", "w"][self.isWhiteTurn]
         selectedPiece = self.state[coord[0]][coord[1]]
-        print(
-            f"# Player: {player}\nSelected piece: {selectedPiece}\nCoord ({coord[0]},{coord[1]})")
+        # print(
+        #     f"# Player: {player}\nSelected piece: {selectedPiece}\nCoord ({coord[0]},{coord[1]})")
 
         if len(self.playerSelections) == 0:
             if selectedPiece[0] == player:
                 self.selectedSq = coord
                 self.playerSelections.append(coord)
         else:
-            if selectedPiece == "--":
-                self.makeMove(Move(self.selectedSq[0],
-                                   self.selectedSq[1], coord[0], coord[1]))
-
-            elif selectedPiece == self.state[self.selectedSq[0]][self.selectedSq[1]]:
+            if selectedPiece == self.state[self.selectedSq[0]][self.selectedSq[1]]:
                 self.selectedSq = None
                 self.playerSelections.pop()
+            else:
+                self.makeMove(Move(self.selectedSq, coord))
+
     '''
     Make a move
     '''
 
     def makeMove(self, move):
-        self.state[move.endRow][move.endCol] = self.state[move.startRow][move.startCol]
-        self.state[move.startRow][move.startCol] = "--"
-        self.selectedSq = None
-        self.playerSelections.pop()
-        self.isWhiteTurn = not self.isWhiteTurn
-        self.logs.append(move)
-
+        if self.isValidMove(move):
+            self.state[move.endRow][move.endCol] = self.state[move.startRow][move.startCol]
+            self.state[move.startRow][move.startCol] = "--"
+            player = ["b", "w"][self.isWhiteTurn]
+            print(f"player: {player}, {move}")
+            self.logs.append(move)
+            self.selectedSq = None
+            self.playerSelections.pop()
+            self.isWhiteTurn = not self.isWhiteTurn
+        else:
+            print("Not a valid move!")
     '''
     undo the last move
     '''
@@ -56,6 +60,71 @@ class GameState():
             self.state[move.endRow][move.endCol] = "--"
             self.isWhiteTurn = not self.isWhiteTurn
 
+    """
+    check for valid moves
+    """
+
+    def isValidMove(self, move):
+        possibleMoves = []
+        pieceName = self.state[move.startRow][move.startCol][1]
+        if pieceName == "p":
+            self.getPawnMoves(move.startRow, move.startCol, possibleMoves)
+        if pieceName == "R":
+            self.geRockMoves(move.startRow, move.startCol, possibleMoves)
+        print(possibleMoves)
+        return (move.endRow, move.endCol) in possibleMoves
+
+    """
+    Get all possible moves for a pawn
+    """
+
+    def getPawnMoves(self, r, c, moves):
+        coef = -1 if self.isWhiteTurn else 1
+        if self.state[r + coef*1][c] == "--":
+            moves.append((r + coef*1, c))
+            if self.isWhiteTurn:
+                if r+coef*2 >= self.dimension // 2:
+                    moves.append((r+coef*2, c))
+            else:
+                if r+coef*2 < self.dimension // 2:
+                    moves.append((r+coef*2, c))
+        if self.state[r + coef*1][c+1] != "--":
+            moves.append((r+coef*1, c+1))
+        if self.state[r + coef*1][c-1] != "--":
+            moves.append((r+coef*1, c-1))
+    """
+    Get all possible moves for a Rock
+    """
+
+    def geRockMoves(self, r, c, moves):
+        player = "w" if self.isWhiteTurn else "b"
+        i = 1
+        while (r+i) in range(0, self.dimension) and self.state[r + i][c] == "--":
+            moves.append((r + i, c))
+            i += 1
+
+        if (r+i) in range(0, self.dimension) and self.state[r + i][c][0] != player:
+            moves.append((r + i, c))
+        i = 1
+        while (r-i) in range(0, self.dimension) and self.state[r - i][c] == "--":
+            moves.append((r - i, c))
+            i += 1
+        if (r-i) in range(0, self.dimension) and self.state[r - i][c][0] != player:
+            moves.append((r - i, c))
+        i = 1
+        while (c+i) in range(0, self.dimension) and self.state[r][c + i] == "--":
+            moves.append((r, c + i))
+            i += 1
+
+        if (c+i) in range(0, self.dimension) and self.state[r][c + i][0] != player:
+            moves.append((r, c + i))
+        i = 1
+        while (c-i) in range(0, self.dimension) and self.state[r][c - i] == "--":
+            moves.append((r, c - i))
+            i += 1
+        if (c-i) in range(0, self.dimension) and self.state[r][c - i][0] != player:
+            moves.append((r, c - i))
+
 
 class Move:
 
@@ -66,12 +135,11 @@ class Move:
                    "e": 4, "d": 3, "c": 2, "b": 1, "a": 0}
     colsToFiles = {k: v for v, k in filesToCols.items()}
 
-    def __init__(self, startRow, startCol, endRow, endCol) -> None:
-        self.startRow = startRow
-        self.startCol = startCol
-        self.endRow = endRow
-        self.endCol = endCol
-        # self.pieceName = pieceName
+    def __init__(self, start, end) -> None:
+        self.startRow = start[0]
+        self.startCol = start[1]
+        self.endRow = end[0]
+        self.endCol = end[1]
 
     def __str__(self) -> str:
         return f"move {self.getRankFile(self.startCol,self.startRow)} to {self.getRankFile(self.endCol,self.endRow)} "
