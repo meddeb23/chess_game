89c0e6701ed7bed560c011a90957e253a6cec092
HeithemTarchouna <75651342+HeithemTarchouna@users.noreply.github.com>
2022-01-19 22:01:26 +0100

    update
    Added bishop moves
    Added queen moves
    Fixed some bugs

--- gameState.py
+++ gameState.py
@@ -16,7 +16,7 @@ class GameState():
             ["--", "--", "--", "--", "--", "--", "--", "--"],
             ["--", "--", "--", "--", "--", "--", "--", "--"],
             ["--", "--", "--", "--", "--", "--", "--", "--"],
-            ["wR", "bp", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
             ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],
             ["wR", "wN", "wB", "wQ", "wk", "wB", "wN", "wR"],
         ]
@@ -29,14 +29,14 @@ class GameState():
     def selectPiece(self, coord):
         player = ["b", "w"][self.isWhiteTurn]
         selectedPiece = self.state[coord[0]][coord[1]]
-        # print(
-        #     f"# Player: {player}\nSelected piece: {selectedPiece}\nCoord ({coord[0]},{coord[1]})")
+        print(f'Player: {player}\nSelected piece: {selectedPiece}\nCoord ({coord[0]},{coord[1]})')
 
         if len(self.playerSelections) == 0:
             if selectedPiece[0] == player:
                 self.selectedSq = coord
                 self.playerSelections.append(coord)
         else:
+            print(self.selectedSq[0], self.selectedSq[1])
             if selectedPiece == self.state[self.selectedSq[0]][self.selectedSq[1]]:
                 self.selectedSq = None
                 self.playerSelections.pop()
@@ -59,6 +59,7 @@ class GameState():
             self.isWhiteTurn = not self.isWhiteTurn
         else:
             print("Not a valid move!")
+
     '''
     undo the last move
     '''
@@ -81,7 +82,8 @@ class GameState():
             "p": self.getPawnMoves,
             "R": self.getRockMoves,
             "N": self.getKnightMoves,
-            "B": self.getPawnMoves,
+            "B": self.getBishopMoves,
+            "Q": self.getQueenMoves
         }
         getPieceMove[pieceName](move.startRow, move.startCol, possibleMoves)
         return move in possibleMoves
@@ -92,18 +94,20 @@ class GameState():
 
     def getPawnMoves(self, r, c, moves):
         coef = -1 if self.isWhiteTurn else 1
-        if self.state[r + coef*1][c] == "--":
-            moves.append(Move((r, c), (r + coef*1, c)))
+        print(c)
+        if self.state[r + coef * 1][c] == "--":
+            moves.append(Move((r, c), (r + coef * 1, c)))
             if self.isWhiteTurn:
-                if r+coef*2 >= self.dimension // 2:
-                    moves.append(Move((r, c), (r+coef*2, c)))
+                if r + coef * 2 >= self.dimension // 2:
+                    moves.append(Move((r, c), (r + coef * 2, c)))
             else:
-                if r+coef*2 < self.dimension // 2:
-                    moves.append(Move((r, c), (r+coef*2, c)))
-        if self.state[r + coef*1][c+1] != "--":
-            moves.append(Move((r, c), (r + coef*1, c+1)))
-        if self.state[r + coef*1][c-1] != "--":
-            moves.append(Move((r, c), (r + coef*1, c-1)))
+                if r + coef * 2 < self.dimension // 2:
+                    moves.append(Move((r, c), (r + coef * 2, c)))
+
+        if self.state[r + coef * 1][c + 1 if c < 7 else c] != "--":
+            moves.append(Move((r, c), (r + coef * 1, c + 1)))
+        if self.state[r + coef * 1][c - 1] != "--":
+            moves.append(Move((r, c), (r + coef * 1, c - 1)))
 
     """
     Get all possible moves for a knight
@@ -114,9 +118,9 @@ class GameState():
         directions = [(2, 1), (2, -1), (-2, 1), (-2, -1),
                       (1, 2), (1, -2), (-1, 2), (-1, -2)]
         for i, j in directions:
-            if r+i in range(self.dimension) and c+j in range(self.dimension):
-                if self.state[r + i][c+j][0] != player:
-                    moves.append(Move((r, c), (r + i, c+j)))
+            if r + i in range(self.dimension) and c + j in range(self.dimension):
+                if self.state[r + i][c + j][0] != player:
+                    moves.append(Move((r, c), (r + i, c + j)))
 
     """
     Get all possible moves for a Rock
@@ -125,34 +129,73 @@ class GameState():
     def getRockMoves(self, r, c, moves):
         player = "w" if self.isWhiteTurn else "b"
         i = 1
-        while (r+i) in range(0, self.dimension) and self.state[r + i][c] == "--":
+        while (r + i) in range(0, self.dimension) and self.state[r + i][c] == "--":
             moves.append(Move((r, c), (r + i, c)))
             i += 1
-        if (r+i) in range(0, self.dimension) and self.state[r + i][c][0] != player:
+        if (r + i) in range(0, self.dimension) and self.state[r + i][c][0] != player:
             moves.append(Move((r, c), (r + i, c)))
         i = 1
-        while (r-i) in range(0, self.dimension) and self.state[r - i][c] == "--":
+        while (r - i) in range(0, self.dimension) and self.state[r - i][c] == "--":
             moves.append(Move((r, c), (r - i, c)))
             i += 1
-        if (r-i) in range(0, self.dimension) and self.state[r - i][c][0] != player:
+        if (r - i) in range(0, self.dimension) and self.state[r - i][c][0] != player:
             moves.append(Move((r, c), (r - i, c)))
         i = 1
-        while (c+i) in range(0, self.dimension) and self.state[r][c + i] == "--":
+        while (c + i) in range(0, self.dimension) and self.state[r][c + i] == "--":
             moves.append(Move((r, c), (r, c + i)))
             i += 1
 
-        if (c+i) in range(0, self.dimension) and self.state[r][c + i][0] != player:
+        if (c + i) in range(0, self.dimension) and self.state[r][c + i][0] != player:
             moves.append(Move((r, c), (r, c + i)))
         i = 1
-        while (c-i) in range(0, self.dimension) and self.state[r][c - i] == "--":
+        while (c - i) in range(0, self.dimension) and self.state[r][c - i] == "--":
             moves.append(Move((r, c), (r, c - i)))
             i += 1
-        if (c-i) in range(0, self.dimension) and self.state[r][c - i][0] != player:
+        if (c - i) in range(0, self.dimension) and self.state[r][c - i][0] != player:
             moves.append(Move((r, c), (r, c - i)))
+
+    def getBishopMoves(self, r, c, moves):
+        enemy_color = "b" if self.isWhiteTurn else "w"
+        directions = [[-1, -1], [-1, 1], [1, 1], [1, -1]]
+        for d in directions:
+            for i in range(1, 8):
+                end_row = r + i * d[0]
+                end_col = c + i * d[1]
+                if end_row in range(8) and end_col in range(8):
+                    end_piece = self.state[end_row][end_col]
+                    if end_piece == "--":  # empty space is valid
+                        moves.append(Move((r, c), (end_row, end_col)))
+                    elif end_piece[0] == enemy_color:
+                        moves.append(Move((r, c), (end_row, end_col)))
+                        break
+                    else:  # friendly piece
+                        # add feature to select friendly pieces
+                        break
+                else:  # off board
+                    break
+
+    def getQueenMoves(self, r, c, moves):
+        enemy_color = "b" if self.isWhiteTurn else "w"
+        directions = [[0, -1], [-1, 0], [0, 1], [1, 0], [-1, -1], [-1, 1], [1, 1], [1, -1]]
+        for d in directions:
+            for i in range(1, 8):
+                end_row = r + i * d[0]
+                end_col = c + i * d[1]
+                if end_row in range(8) and end_col in range(8):
+                    end_piece = self.state[end_row][end_col]
+                    if end_piece == "--":  # empty space is valid
+                        moves.append(Move((r, c), (end_row, end_col)))
+                    elif end_piece[0] == enemy_color:
+                        moves.append(Move((r, c), (end_row, end_col)))
+                        break
+                    else:  # friendly piece
+                        # add feature to select friendly pieces
+                        break
+                else:  # off board
+                    break
 
 
 class Move:
-
     ranksToRow = {"1": 7, "2": 6, "3": 5,
                   "4": 4, "5": 3, "6": 2, "7": 1, "8": 0}
     rowsToRanks = {k: v for v, k in ranksToRow.items()}
@@ -170,7 +213,7 @@ class Move:
         return __o.startRow == self.startRow and __o.startCol == self.startCol and __o.endRow == self.endRow and __o.endCol == self.endCol
 
     def __str__(self) -> str:
-        return f"move {self.getRankFile(self.startCol,self.startRow)} to {self.getRankFile(self.endCol,self.endRow)} "
+        return f"move {self.getRankFile(self.startCol, self.startRow)} to {self.getRankFile(self.endCol, self.endRow)} "
 
     def getRankFile(self, c, r):
-        return self.colsToFiles[c]+self.rowsToRanks[r]
+        return self.colsToFiles[c] + self.rowsToRanks[r]
